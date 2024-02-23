b48f0ec6a9a99decbb5a6c1884f57b8a31f31a4c
Gabriel Eriife John <eriifejohn@gmail.com>
2024-02-23 17:06:08 +0100

    added the local storage feature

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-let items = JSON.parse(localStorage.getItem("todo-list")) || []
-let textInputEl = document.getElementById("textInput")
-const todoList = document.getElementById("todo-list")
+
 const taskBtn = document.getElementById("add-task")
 
 
@@ -23,48 +21,115 @@ const taskBtn = document.getElementById("add-task")
 
 
 
-function add(){
+// function add(){
 
-    if (textInputEl.value){
+//     if (textInputEl.value){
           
-           let item = textInputEl.value
-           const listItem = document.createElement("li")
-            listItem.classList.add("list-item")
-           const label = document.createElement("label")
-           const checkbox = document.createElement("input")
-             checkbox.type = "checkbox"
-           const textNode = document.createElement("span")
-                  textNode.textContent = item
-           const deleteIcon = document.createElement("i")
-                 deleteIcon.className = "fa-solid fa-trash-can delete-icon" 
- 
-                
-             
-           listItem.appendChild(label)      
-           listItem.appendChild(checkbox)
-           listItem.appendChild(textNode)
-           listItem.appendChild(deleteIcon)
+//            let item = textInputEl.value
+//            const listItem = document.createElement("li")
+//             listItem.classList.add("list-item")
+//            const label = document.createElement("label")
+//            const checkbox = document.createElement("input")
+//              checkbox.type = "checkbox"
+//            const textNode = document.createElement("span")
+//                   textNode.textContent = item
+//            const deleteIcon = document.createElement("i")
+//                  deleteIcon.className = "fa-solid fa-trash-can delete-icon"              
+//            listItem.appendChild(label)      
+//            listItem.appendChild(checkbox)
+//            listItem.appendChild(textNode)
+//            listItem.appendChild(deleteIcon)
 
-           todoList.appendChild(listItem)
-           textInputEl.value = ""
+//            todoList.appendChild(listItem)
+//            textInputEl.value = ""
            
-           checkbox.addEventListener("change", function(){
+//            checkbox.addEventListener("change", function(){
             
            
-            if(checkbox.checked){
-                textNode.classList.add("strike-through")
-            } else {
-              textNode.classList.remove("strike-through")
-            }
-           })
+//             if(checkbox.checked){
+//                 textNode.classList.add("strike-through")
+//             } else {
+//               textNode.classList.remove("strike-through")
+//             }
+//            })
         
          
            
-           deleteIcon.addEventListener("click", function(){
-            todoList.removeChild(listItem)
-         }) 
+//            deleteIcon.addEventListener("click", function(){
+//             todoList.removeChild(listItem)
+//          }) 
     
-    }
+//     }
 
 
+// }
+
+
+function add() {
+
+  const textInputEl = document.getElementById("textInput")
+  if (textInputEl.value){
+        const todoList = document.getElementById("todo-list")
+        let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+        tasks.push(textInputEl.value)
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+        textInputEl.value = ''
+  }
+
+render()
 }
+
+
+
+
+function removeTasks(taskIndex){
+  
+  let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+  tasks.splice(taskIndex, 1)
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
+render()
+}
+
+  
+function render() {
+
+  const todoList = document.getElementById("todo-list")
+  todoList.innerHTML = ""
+  let tasks = JSON.parse(localStorage.getItem('tasks')) ||[]
+
+  tasks.forEach(task => {
+   const listItem = document.createElement("li")
+   listItem.classList.add("list-item")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+  const textNode = document.createElement("span")
+         textNode.textContent = task
+         const deleteIcon = document.createElement("i")
+  deleteIcon.className = "fa-solid fa-trash-can delete-icon"
+  deleteIcon.addEventListener("click", removeTasks)
+
+        checkbox.addEventListener("change", function(){
+       
+         if(checkbox.checked){
+             textNode.classList.add("strike-through")
+         } else {
+           textNode.classList.remove("strike-through")
+         }
+        })
+    
+      
+     
+
+  listItem.appendChild(label)      
+  listItem.appendChild(checkbox)
+  listItem.appendChild(textNode)
+  listItem.appendChild(deleteIcon)
+
+  todoList.appendChild(listItem)
+   
+  });
+  
+}
+render()
